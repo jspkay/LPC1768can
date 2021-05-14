53e467686245017e3d9996e54a777958d068de42
mrwasp98 <antoves1998@gmail.com>
2021-05-14 16:10:25 +0200

    IRQ_CAN

--- CAN/IRQ_CAN.c
+++ CAN/IRQ_CAN.c
@@ -3,13 +3,32 @@
 
 void IRQ_CAN1(void);
 void IRQ_CAN2(void);
+extern int bus_mine;
 
+
+void panic(){}
+	
 void CAN_IRQHandler (void)
 {
 	if( (LPC_CAN1->GSR & 1) == 1) IRQ_CAN1();
 	else if( (LPC_CAN2->GSR & 1) == 1) IRQ_CAN2();
+
+	LPC_CAN_TypeDef* can;
+	can = LPC_CAN1;
 	
-	// TODO: clear interrupt request
+	//gestione errori
+	if((can->GSR & CAN_GSR_DOS) == CAN_GSR_DOS){ //OVERRUN
+		panic();
+	}
+	if((can->ICR & CAN_ICR_WUI) == CAN_ICR_WUI){ //DETECTED BUS ACTIVITY
+		panic();
+	}
+	if((can->ICR & CAN_ICR_EPI) == CAN_ICR_EPI){ //TOO MUCH ERRORS
+		panic();
+	}
+	if((can->ICR & CAN_ICR_ALI) == CAN_ICR_ALI){ //ARBITRATION LOST
+		bus_mine=0;
+	}
 }
 
 void IRQ_CAN1(){
