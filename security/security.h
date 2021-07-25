d9f94d359361d9e681ac4314d24a5ceea1b99672
jspkay <jspkay@gmail.com>
2021-07-25 16:25:30 +0200

    security updated

--- security/security.h
+++ security/security.h
@@ -4,12 +4,15 @@
 /*************************** HEADER FILES ***************************/
 #include <stddef.h>
 #include "sha256.h"
+#incude "aes.h"
 
 #define ENCRYPT 1
 #define DECRYPT 0
 
-void DES3(unsigned char* data, unsigned char key[3][8], int mode);
+void DES3(unsigned char* data, unsigned char **key, int mode);
 void digest(unsigned char *data, unsigned char *key, unsigned char *final);
 int verify_digest(unsigned char *data, unsigned char* key, unsigned char* dig);
+void AES(struct AES_ctx* ctx, uint8_t *data);
+struct AES_ctx* AES_init(const uint8_t *key, const uint8_t iv);
 
 #endif   // SECURITY_H
